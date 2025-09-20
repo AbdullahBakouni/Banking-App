8a08a7ff72e9c46fd2e726b03383e3dd50887a84
Abdullah Bakouni <aboodbak70@gmail.com>
2025-09-20 13:15:51 +0300

    implements the main page structure and add some ts types

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,38 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home: React.FC = () => {
+  const loggedin = {
+    firstName: "Abdullah",
+    lastName: "Bakouni",
+    email: "aboodbak70@gmail.com",
+  };
   return (
-    <>
-      <div className="bg-pink-500 font-inter">Hi</div>
-    </>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedin?.firstName || "Guest"}
+            subtext="Access and Manage your account and transaction efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.76}
+          />
+        </header>
+        {/*Recent Transcations*/}
+      </div>
+      <RightSidebar
+        user={loggedin}
+        transactions={[]}
+        banks={[{ currentBalance: 124.5 }, { currentBalance: 125.69 }]}
+      />
+    </section>
   );
 };
 
